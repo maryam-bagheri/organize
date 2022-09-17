722931bc4d3b0c197963aa6adbf78d1eb4084393
maryam-bagheri <mbagheri6895@gmail.com>
2022-09-17 11:58:31 +0430

    exec bug fix

---
+++ project/file_organizer_strategy/exec_organizer.py
@@ -0,0 +1,4 @@
+from file_organizer_strategy.file_organizer import file_organizer
+class exec_organizer(file_organizer):
+    def __init__(self):
+        super().__init__()
--- project/file_organizer_strategy/file_orgabnizer_strategy.py
+++ project/file_organizer_strategy/file_orgabnizer_strategy.py
@@ -8,6 +8,7 @@ from project_enum.file_organizer_type import file_organizer_type
 from file_organizer_strategy.image_organizer import image_organizer
 from project_enum.pdf_extension import pdf_extension
 from project_enum.exec_extension import exec_extension
+
 class file_organizer_strategy:
     def choose_strategy(self,file,strategy_type,folder):
         if True in [e.name==strategy_type for e in image_extension]:
