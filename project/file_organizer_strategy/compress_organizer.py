566da104c67c479d6ed69c1128d5bca0f36a78aa
maryam-bagheri <mbagheri6895@gmail.com>
2022-09-22 14:18:52 +0330

    fix audio

--- project/file_organizer_strategy/compress_organizer.py
+++ project/file_organizer_strategy/compress_organizer.py
@@ -1,6 +1,5 @@
 from file_organizer_strategy.file_organizer import file_organizer
 
-
 class compress_organizer(file_organizer):
     def __init__(self):
         super().__init__()
--- project/file_organizer_strategy/pdf_organizer.py
+++ project/file_organizer_strategy/pdf_organizer.py
@@ -1,6 +1,5 @@
 from file_organizer_strategy.file_organizer import file_organizer
 
-
 class pdf_organizer(file_organizer):
     def __init__(self):
         super.__init__()
