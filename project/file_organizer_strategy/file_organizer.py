a0a62f28d216aac817275a08aa05edbf3b1a16c3
maryam-bagheri <mbagheri6895@gmail.com>
2022-09-23 23:55:36 +0330

    need to improve

--- project/file_organizer_strategy/file_organizer.py
+++ project/file_organizer_strategy/file_organizer.py
@@ -1,10 +1,14 @@
 from abc import ABC, abstractmethod
 from shutil import move
+import os
 
 class file_organizer:
     def file_mover(self,file,new_location):
         #print(file)
-        move(file, new_location)
+        try:
+            move(os.path.join(file), os.path.join(new_location))
+        except:
+            os.replace(file, new_location)
 
     if __name__=='__main__':
         print('Please run from main')
